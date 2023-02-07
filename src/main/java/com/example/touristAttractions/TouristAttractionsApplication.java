28ee0da347b61922a07acf59aaac6d425e1d9d83
ahmedmahovac <amahovac1@etf.unsa.ba>
2023-02-07 14:30:20 +0100

    add mysql connection

---
+++ src/main/java/com/example/touristAttractions/TouristAttractionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.touristAttractions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristAttractionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristAttractionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/touristAttractions/TouristAttractionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.touristAttractions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristAttractionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
